3336c8eba248404381639eaf392e50d55fc8bb76
MariasRochas <132393830+MariasRochas@users.noreply.github.com>
2023-09-24 13:49:34 -0300

    Add files via upload

---
+++ chat_page.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAs9uTkZ7bm2vJ7_X7I-DB4i6c18IhSH0M",
+    authDomain: "p-c53e2.firebaseapp.com",
+    databaseURL: "https://p-c53e2-default-rtdb.firebaseio.com",
+    projectId: "p-c53e2",
+    storageBucket: "p-c53e2.appspot.com",
+    messagingSenderId: "641404643536",
+    appId: "1:641404643536:web:a774318cd87c2a374d56d5"
+  };
+
+  user_name
+  room_name
+  
+  function send() {
+    firebase.database().ref(room_name).push({
+        name:user_name,
+        message:msg,
+        like:0
+    });
+  }
